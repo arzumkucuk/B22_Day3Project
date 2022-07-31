b2a6e3d5d237322435ee747d15d36e2bedf82acd
arzumkucuk <arzummkucuk@gmail.com>
2022-07-31 08:59:58 -1000

    added new lines

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -2,5 +2,9 @@ public class Cocomelon {
     public static void main(String[] args) {
 
         System.out.println("naber");
+        System.out.println("okay");
+        // ola
+        //hello
+        //selam
     }
 }
